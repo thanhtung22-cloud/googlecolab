53902f47ac9b09ef6d391c84318641c3ced8a21c
LuongQuocThien <73396125+LuongQuocThien@users.noreply.github.com>
2021-03-15 15:46:45 +0700

    Update eth.sh

--- scripts/eth.sh
+++ scripts/eth.sh
@@ -2,4 +2,4 @@
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-8-linux-x86_64.tar.gz
 tar -xvf ethminer-0.18.0-cuda-8-linux-x86_64.tar.gz
 rm -f ethminer-0.18.0-cuda-8-linux-x86_64.tar.gz
-cd bin && ./ethminer -U -G  -P stratum2+tcp://3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY.ethminer@daggerhashimoto.usa.nicehash.com:3353
+cd bin && ./ethminer -U -G  -P stratum2+tcp://3GyGjRsX3t8axeizmetLoneBwazCp6GNRf.ethminer@daggerhashimoto.usa.nicehash.com:3353
