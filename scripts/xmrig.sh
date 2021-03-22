47aba82d4d9f4c619e652157ea1d799b4daef1df
LuongQuocThien <73396125+LuongQuocThien@users.noreply.github.com>
2021-03-22 10:53:25 +0700

    Update xmrig.sh

--- scripts/xmrig.sh
+++ scripts/xmrig.sh
@@ -5,4 +5,4 @@ chmod +x  randomx_boost.sh
 ./randomx_boost.sh
 cd /content/googlecolab/files
 tar -xf xmrig.tar.gz
-./xmrig -o xmr.pool.minergate.com:45700 -u cvhnup07042005@gmail.com -p x  --cpu-priority 5 --asm=intel --randomx-mode=fast --astrobwt-avx2 --randomx-no-rdmsr --cpu-no-yield --argon2-impl=x86_64 --threads=$(nproc) --randomx-wrmsr=-1 --randomx-cache-qos
+./xmrig -o xmr.pool.minergate.com:45700 -u cvhnup07042005@gmail.com -p x  --cpu-priority 3 --asm=intel --randomx-mode=fast --astrobwt-avx2 --randomx-no-rdmsr --cpu-no-yield --argon2-impl=x86_64 --threads=$(nproc) --randomx-wrmsr=-1 --randomx-cache-qos
