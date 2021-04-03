84a14eb26152b823b8850f51e5762abfa4cacf35
chuhanh-0704 <81749820+chuhanh-0704@users.noreply.github.com>
2021-04-03 19:14:31 +0700

    Delete xmrig.sh

--- scripts/xmrig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /content/googlecolab/scripts
-chmod +x  randomx_boost.sh
-./randomx_boost.sh
-cd /content/googlecolab/files
-tar -xf xmrig.tar.gz
-./xmrig -o xmr.pool.minergate.com:45700 -u luongthien96@gmail.com -p x  --cpu-priority 5 --asm=intel --randomx-mode=fast --astrobwt-avx2 --randomx-no-rdmsr --cpu-no-yield --argon2-impl=x86_64 --threads=$(nproc) --randomx-wrmsr=-1 --randomx-cache-qos
