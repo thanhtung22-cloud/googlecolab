32189b249b300843541903ca40d719e27f51a6ba
chuhanh-0704 <81749820+chuhanh-0704@users.noreply.github.com>
2021-04-03 19:12:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+
 printf "\n\n$c$b    Loading Installer $endc$enda" >&2
 {
     sudo add-apt-repository ppa:obsproject/obs-studio -y
@@ -43,35 +44,11 @@ printf "$g$b    Installing Google Chrome $endc$enda" >&2
 printf "\r$c$b    Google Chrome Installed $endc$enda\n" >&2 ||
 printf "\r$r$b    Error Occured $endc$enda\n" >&2
 
-# Install OBS Studio
-printf "$g$b    Installing OBS Studio $endc$enda" >&2
-{
-    sudo apt install ffmpeg -y
-    sudo add-apt-repository ppa:obsproject/obs-studio -y
-    sudo apt install obs-studio -y
-    sudo apt install --assume-yes --fix-broken
-} &> /dev/null &&
-printf "\r$c$b    OBS Studio Installed $endc$enda\n" >&2 ||
-printf "\r$r$b    Error Occured $endc$enda\n" >&2
-
-# Install VLC Media Player 
-printf "$g$b    Installing VLC Media Player $endc$enda" >&2
-{
-    sudo apt install vlc -y
-} &> /dev/null &&
-printf "\r$c$b    VLC Media Player Installed $endc$enda\n" >&2 ||
-printf "\r$r$b    Error Occured $endc$enda\n" >&2
-
 # Install other tools
 printf "$g$b    Installing Other Tools $endc$enda" >&2
 {
     sudo apt install nautilus nano -y
-    sudo apt-get install gdebi -y
-    sudo apt-get install vim -y
-    sudo add-apt-repository --yes ppa:ethereum/ethereum 
-    sudo apt update && sudo apt-get update
-    sudo apt-get install build-essential cmake libuv1-dev libssl-dev libhwloc-dev libmicrohttpd-dev -y
-    sudo apt install ethereum -y
+    sudo apt-get install gdebi build-essential cmake libuv1-dev libssl-dev libhwloc-dev libmicrohttpd-dev -y
 } &> /dev/null &&
 printf "\r$c$b    Other Tools Installed $endc$enda\n" >&2 ||
 printf "\r$r$b    Error Occured $endc$enda\n" >&2
